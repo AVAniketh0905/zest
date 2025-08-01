ea1be51fb3923960fad53506c49636c0c2e54a75
AVAniketh0905 <adimoolamaniketh@gmail.com>
2025-08-01 16:42:29 +0530

    no changes

--- cmd/root.go
+++ cmd/root.go
@@ -72,16 +72,11 @@ Each workspace can be initialized with custom templates for different use cases.
 		Version: VERSION,
 	}
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.zest/zest.yaml)")
 
 	// custom path to zest directory
 	rootCmd.PersistentFlags().StringVar(&utils.CustomZestDir, "custom", "", "custom zest directory (default is $HOME/.zest)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 	// Version
